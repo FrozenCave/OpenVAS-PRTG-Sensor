9a8c6f8cfb911f4cfcaf292f04c32faa687be216
saueregt <timon.saueregger@edu.htl-villach.at>
2023-08-24 12:20:45 +0200

    Cleanup remove Channel, change Limit

--- sensor.py
+++ sensor.py
@@ -1,7 +1,6 @@
 import json
 import sys
 import requests
-
 from paesslerag_prtg_sensor_api.sensor.result import CustomSensorResult
 from paesslerag_prtg_sensor_api.sensor.units import ValueUnit
 
@@ -12,9 +11,9 @@ def make_request(url):
     except requests.RequestException as e:
         return str(e)
 
-if __name__ == "__main__":
+def create_res():
     try:
-        target_url = "http://localhost:8000"
+        target_url = "http://127.0.0.1:8000"
         response_text = make_request(target_url)
 
         try:
@@ -22,49 +21,41 @@ if __name__ == "__main__":
         except ValueError:
             response_value = None
 
-        csr = CustomSensorResult(text="OpenVAS Sensor")
+        csr = CustomSensorResult(text="No Vulnerabilties found")
 
         if response_value is not None:
-            csr.add_primary_channel(name="Out of Range",
-                                    value=response_value,
-                                    unit=ValueUnit.CUSTOM,
-                                    is_float=False,
-                                    is_limit_mode=True,
-                                    limit_min_error=0,
-                                    limit_max_error=12,
-                                    limit_error_msg="Status out of range")
-
-            csr.add_channel(name="Low Priority Vulnerability",
+            csr.add_primary_channel(name="Low Priority Vulnerability",
                                 value=response_value,
                                 unit=ValueUnit.CUSTOM,
-                                is_float=False, 
+                                is_float=True, 
                                 is_limit_mode=True,
-                                limit_min_warning=5.9,
+                                limit_max_warning=2,
                                 limit_warning_msg="Low Priority Vulnerability found")
-                
-                
+                      
             csr.add_channel(name="Crucial Vulnerability",
                                 value=response_value,
                                 unit=ValueUnit.CUSTOM,
-                                is_float=False,
+                                is_float=True,
                                 is_limit_mode=True,
-                                limit_max_error=6,
+                                limit_max_error=5.9,
                                 limit_error_msg="Crucial Vulnerability found")
-
-            if response_value == 'OK':
-                csr.text = "Sensor is green"
 
         else:
             csr.error = "Invalid response: " + response_text
 
-        print(csr.json_result)
+        return csr.json_result
 
     except requests.RequestException:
         csr = CustomSensorResult(text="OpenVAS Endpoint not reachable")
         csr.error = "OpenVAS Endpoint not reachable"
-        print(csr.json_result)
+        return csr.json_result
 
     except Exception as e:
         csr = CustomSensorResult(text="Python Script execution error")
         csr.error = "Python Script execution error: %s" % str(e)
-        print(csr.json_result)
+        return csr.json_result
+
+if __name__ == "__main__":
+    output = create_res()
+
+    print(output)
